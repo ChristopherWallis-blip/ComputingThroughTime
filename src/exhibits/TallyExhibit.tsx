d0906f28da529b1521c53c68850e015f77b6b75f
Christopher Wallis <wallisct@appstate.edu>
2026-08-05 17:23:05 -0400

    Cleaned up the code a bunch

--- src/exhibits/TallyExhibit.tsx
+++ src/exhibits/TallyExhibit.tsx
@@ -13,29 +13,29 @@ export default function TallyCounter() {
 			<div className="exhibit-tallies-modify">
 				<h2> Add </h2>
 
-				<button onClick={() => setCount(count + 1)}>  1  </button>
-				<button onClick={() => setCount(count + 5)}>  5  </button>
-				<button onClick={() => setCount(count + 25)}> 25 </button>
-				<button onClick={() => setCount(count + 50)}> 50 </button>
+				<button onClick={() => setCount(count + 1)}>   1   </button>
+				<button onClick={() => setCount(count + 5)}>   5   </button>
+				<button onClick={() => setCount(count + 25)}>  25  </button>
+				<button onClick={() => setCount(count + 50)}>  50  </button>
 				<button onClick={() => setCount(count + 100)}> 100 </button>
 			</div>
 			<div className="exhibit-tallies-modify">
 				<h2> Subtract </h2>
 
-				<button onClick={() => setCount(Math.max(0, count - 1))}>  1  </button>
-				<button onClick={() => setCount(Math.max(0, count - 5))}>  5  </button>
-				<button onClick={() => setCount(Math.max(0, count - 25))}> 25 </button>
-				<button onClick={() => setCount(Math.max(0, count - 50))}> 50 </button>
+				<button onClick={() => setCount(Math.max(0, count - 1))}>   1   </button>
+				<button onClick={() => setCount(Math.max(0, count - 5))}>   5   </button>
+				<button onClick={() => setCount(Math.max(0, count - 25))}>  25  </button>
+				<button onClick={() => setCount(Math.max(0, count - 50))}>  50  </button>
 				<button onClick={() => setCount(Math.max(0, count - 100))}> 100 </button>
 			</div>
 
 			<div className="exhibit-tallies">
 				{Array.from({ length: groups }, (_, i) => (
 					<div className="tally-group" key={i}>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
+						<span />
+						<span />
+						<span />
+						<span />
 						<span className="tally-slash"></span>
 					</div>
 				))}
